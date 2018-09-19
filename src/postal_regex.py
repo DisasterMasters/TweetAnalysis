bef9367e94d7808f9f7bd60eea09986e83cb6387
Nicholas West <nwest13@vols.utk.edu>
2018-09-19 09:37:15 -0400

    Refined regexes, added basic geolocation lookup (using Geopy)

--- src/postal_regex.py
+++ src/postal_regex.py
@@ -3,9 +3,10 @@ import re
 import sys
 
 #import tweepy
+from geopy.geocoders import Nominatim
 
 # Obtained from <https://pe.usps.com/text/pub28/28apc_002.htm>
-STREET_SUFFIXES = {
+STREET_SUFFIXES = frozenset({
     'ALLEE', 'ALLEY', 'ALLY', 'ALY', 'ANEX', 'ANNEX', 'ANNX', 'ANX', 'ARC',
     'ARCADE', 'AV', 'AVE', 'AVEN', 'AVENU', 'AVENUE', 'AVN', 'AVNUE', 'BAYOO',
     'BAYOU', 'BCH', 'BEACH', 'BEND', 'BG', 'BGS', 'BLF', 'BLFS', 'BLUF',
@@ -71,56 +72,84 @@ STREET_SUFFIXES = {
     'VILLIAGE', 'VIS', 'VIST', 'VISTA', 'VL', 'VLG', 'VLGS', 'VLLY', 'VLY',
     'VLYS', 'VST', 'VSTA', 'VW', 'VWS', 'WALK', 'WALKS', 'WALL', 'WAY',
     'WAYS', 'WELL', 'WELLS', 'WL', 'WLS', 'WY', 'XING', 'XRD', 'XRDS'
-}
+})
 
-STATE_INITIALS = {
+STATE_INITIALS = frozenset({
     'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID',
     'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS',
     'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
     'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV',
     'WI', 'WY', 'AS', 'DC', 'FM', 'GU', 'MH', 'MP', 'PW', 'PR', 'VI'
-}
+})
 
-StreetAddress = collections.namedtuple('StreetAddress', 'house_number street city state zip_code')
+class StreetAddress(collections.namedtuple('StreetAddress', 'house_number street city state zip_code')):
+    @staticmethod
+    def match(text):
+        LINE1REGEX = re.compile(r'(?P<house_number>[0-9]{1,10}) (?P<street>([A-Z][a-z]* ?)+? (?P<street_suffix>[a-z]+))')
+        LINE2REGEX = re.compile(r'(?P<city>([A-Z][a-z]+ ?)+?), (?P<state>[A-Z]{2})( (?P<zip_code>[0-9]{5}(-[0-9]{4})?))?')
 
-def search_for_address(text):
-    LINE1REGEX = re.compile(r'(?P<house_number>[0-9]{1,10}) (?P<street>[a-z ]+? (?P<street_suffix>[a-z]+))', re.IGNORECASE)
-    LINE2REGEX = re.compile(r'(?P<city>([A-Z][a-z]+ ?)+?), (?P<state>[A-Z]{2})( (?P<zip_code>[0-9]{5}(-[0-9]{4})?))?')
+        line1 = LINE1REGEX.search(text)
+        line2 = LINE2REGEX.search(text)
 
-    line1 = LINE1REGEX.search(text)
-    line2 = LINE2REGEX.search(text)
+        house_number = None
+        street       = None
+        city         = None
+        state        = None
+        zip_code     = None
 
-    house_number = None
-    street       = None
-    city         = None
-    state        = None
-    zip_code     = None
+        if line1 is not None:
+            if line1.group('street_suffix').upper() in STREET_SUFFIXES:
+                house_number  = line1.group('house_number')
+                street        = line1.group('street')
+            else:
+                line1 = None
 
-    if line1 is not None:
-        if line1.group('street_suffix').upper() in STREET_SUFFIXES:
-            house_number  = line1.group('house_number')
-            street        = line1.group('street')
-        else:
-            line1 = None
+        if line2 is not None:
+            if line2.group('state') in STATE_INITIALS:
+                city     = line2.group('city')
+                state    = line2.group('state')
+                zip_code = line2.group('zip_code')
+            else:
+                line2 = None
 
-    if line2 is not None:
-        if line2.group('state') in STATE_INITIALS:
-            city     = line2.group('city')
-            state    = line2.group('state')
-            zip_code = line2.group('zip_code')
-        else:
-            line2 = None
+        if line1 is None and line2 is None:
+            return None
 
-    if line1 is None and line2 is None:
+        return StreetAddress(
+            house_number = house_number,
+            street = street,
+            city = city,
+            state = state,
+            zip_code = zip_code
+        )
+
+    def __str__(self):
+        addrstr = ""
+
+        if addr.street is not None:
+            addrstr += addr.house_number + " " + addr.street
+
+            if addr.city is not None:
+                addrstr += ", "
+
+        if addr.city is not None:
+            addrstr += addr.city + ", " + addr.state
+
+            if addr.zip_code is not None:
+                addrstr += " " + addr.zip_code
+
+        return addrstr
+
+
+    def geocode(self, geolocator):
+        results = geolocator.geocode(str(self), exactly_one = False)
+
+        if results is not None:
+            for coord in results:
+                if "USA" in coord.address:
+                    return (coord.latitude, coord.longitude)
+
         return None
-
-    return StreetAddress(
-        house_number = house_number,
-        street = street,
-        city = city,
-        state = state,
-        zip_code = zip_code
-    )
 
 if len(sys.argv) < 2:
     print("Usage: " + sys.argv[0] + " [input_file]")
@@ -128,6 +157,9 @@ if len(sys.argv) < 2:
 
 all_tweets = []
 tweets_w_addrs = []
+tweets_w_coords = []
+
+geolocator = Nominatim(user_agent = "disaster-masters-curent")
 
 with open(sys.argv[1]) as fd:
     for tweet in fd.read().split('\t'):
@@ -141,7 +173,7 @@ with open(sys.argv[1]) as fd:
         text = tweet[0]
         link = tweet[2]
 
-        addr = search_for_address(text)
+        addr = StreetAddress.match(text)
 
         if addr is not None:
             tweet = tweet.copy()
@@ -149,12 +181,23 @@ with open(sys.argv[1]) as fd:
 
             tweets_w_addrs.append(tweet)
 
+            coords = addr.geocode(geolocator)
+
+            if coords is not None:
+                tweet = tweet.copy()
+                tweet.append(coords)
+
+                tweets_w_coords.append(tweet)
+
 '''
 Don't do anything yet with the data, except print the percentage of tweets with
 potential address information to stdout. We are going to need access to the
-Google Maps API (which has a monthy fee) and the Twitter API (which needs to be
-approved) before we can do anything else.
+Twitter API (which needs to be approved) before we can do anything else.
 '''
-print("Total tweets                         : %d" % len(all_tweets))
-print("Tweets With an address in their text : %d" % len(tweets_w_addrs))
-print("Percentage                           : %f %%" % (100 * len(tweets_w_addrs) / len(all_tweets)))
+print("Total tweets                                : %d" % len(all_tweets))
+print("---------------------------------------------")
+print("Tweets with an address in their text        : %d" % len(tweets_w_addrs))
+print("Percentage                                  : %f %%" % (100 * len(tweets_w_addrs) / len(all_tweets)))
+print("---------------------------------------------")
+print("Tweets whose address is a valid geolocation : %d" % len(tweets_w_coords))
+print("Percentage                                  : %f %%" % (100 * len(tweets_w_coords) / len(all_tweets)))
