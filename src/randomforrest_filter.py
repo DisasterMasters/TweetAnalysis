0e6ecd4b5e5ec1a0f59742cae230345d3ebaf218
Manny Bhidya <abhidya@vols.utk.edu>
2018-10-10 14:31:12 -0400

    updated filter to train tfidf only on corpus

--- src/randomforrest_filter.py
+++ src/randomforrest_filter.py
@@ -1,21 +1,32 @@
+import glob
+from collections import Counter
+
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 
 
 def build_model(verbose=False):
     if (verbose):
         print("Loading Training Data")
 
-    data_source = r"/home/manny/PycharmProjects/TweetAnalysis/DATA/T-06-Hurricane_Sandy_Labeled/2012_Sandy_Hurricane-ontopic_offtopic.csv"
+    data_source = r"/home/manny/PycharmProjects/TweetAnalysis/DATA/T-06-Hurricane_Sandy_Labeled/"
 
-    train = pd.read_csv(data_source, names=['tweet id', 'tweet', 'label'])
+    training_list = []
+    files = glob.glob(data_source + "/*.csv")
+    for fname in files:
+        df = pd.read_csv(fname, index_col=None, header=0)
+        training_list.append(df)
 
-    train['label'] = train['label'].map({'on-topic': int(1), 'off-topic': int(0)})
-    train['label'] = train['label'].fillna(value=0)
-    y = train['label']
-    x = train['tweet'].fillna(" ")
+    train = pd.concat(training_list, sort=False, ignore_index=True)
+    training_list = []
+
+    # print(Counter(train['Label']))
+
+    train['Label'] = train['Label'].fillna(value=0)
+    y = train['Label']
+    x = train['Tweet'].fillna(" ")
+
     if (verbose):
         print("Loading Corpus")
     corpus = pd.read_csv('corpus.csv')
@@ -40,28 +51,28 @@ def build_model(verbose=False):
 
     if (verbose):
 
-        clf = RandomForestClassifier(verbose=2,  max_features=.2, n_estimators=12, min_samples_leaf=3)
+        clf = RandomForestClassifier(verbose=2, max_features=.2, n_estimators=12, min_samples_leaf=3, n_jobs=-1)
     else:
 
-        clf = RandomForestClassifier(verbose=0, max_features=.2, n_estimators=12, min_samples_leaf=3)
+        clf = RandomForestClassifier(verbose=0, max_features=.2, n_estimators=12, min_samples_leaf=3, n_jobs=-1)
 
     clf.fit(x, y)
 
-    sample_source = r"/home/manny/PycharmProjects/TweetAnalysis/src/Sample.csv"
-
-    test = pd.read_csv(sample_source)
-
-    test['Manual'] = test['Manual'].fillna(value=0)
-    y = test['Manual']
-    x = test['Tweet'].fillna(" ")
-    if (verbose):
-        print("Loading Validation set")
-
-    x = tfidf.transform(x).toarray()
-
-    print(clf.score(x, y))
+    # sample_source = r"/home/manny/PycharmProjects/TweetAnalysis/DATA/T-06-Hurricane_Sandy_Labeled/Manually_labelled_data_2.csv"
+    #
+    # test = pd.read_csv(sample_source)
+    #
+    # test['Label'] = test['Label'].fillna(value=0)
+    # # y = test['Label']
+    # x = test['Tweet'].fillna(" ")
+    # if (verbose):
+    #     print("Loading Validation set")
+    #
+    # x = tfidf.transform(x).toarray()
+    #
+    # print(clf.score(x, y))
     return tfidf, clf
-
+#
 
 tfidf, clf = build_model(True)
 
