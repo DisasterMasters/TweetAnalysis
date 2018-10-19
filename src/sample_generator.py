f8a97c628a0dbccbafc3db79953c4a13df55c2f2
Manny Bhidya <abhidya@vols.utk.edu>
2018-10-19 11:15:44 -0400

    updated to make sure that already manually labelled data does not get included in sample

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -1,41 +1,34 @@
 import pandas as pd
-from collections import Counter
-import numpy as np
 import randomforrest_filter
+import glob
 
 tfidf, clf = randomforrest_filter.build_model(True)
 # rfresults = clf.predict(tfidf.transform(corpus))
 
+data_source = r"/home/manny/PycharmProjects/TweetAnalysis/DATA/T-06-Hurricane_Sandy_Labeled/"
 
+training_list = []
+files = glob.glob(data_source + "/*.csv")
+for fname in files:
+    df = pd.read_csv(fname, index_col=None, header=0)
+    training_list.append(df)
+
+train = pd.concat(training_list, sort=False, ignore_index=True)
 
 corpus = pd.read_csv('corpus.csv')
 
-
 corpus = corpus.dropna()
-# print(corpus['Tweet'])
-x = tfidf.transform(corpus['Tweet'])
+
+common = corpus.merge(train, ignore_index=True)
+
+new = corpus[(~corpus.Tweet.isin(common.Tweet)) & (~corpus.Tweet.isin(common.Tweet))]
+x = tfidf.transform(new['Tweet'])
 print(x.shape)
 rfresults = clf.predict(x)
 
-
-print(rfresults)
-print(Counter(rfresults))
-
-
-from crisislex import Crisislex
-
-c_lex = Crisislex()
-lex = c_lex.lex
-
-lexresults = corpus['Tweet'].apply(lambda x: '1.0' if any((txt in x) for txt in lex) else '0.0')
-
-print(Counter(lexresults))
-
-
-print(type(corpus))
-lexresults = pd.Series(lexresults, name="Lex")
 rfresults = pd.Series(rfresults, name="RF")
-results = pd.concat([corpus, rfresults, lexresults], axis=1)
+results = pd.concat([new, rfresults], axis=1, ignore_index=True)
+print(results.head())
 
-
-results.sample(500).to_csv("Sample1.csv")
+# results = results[results.Tweet != 0]
+results.sample(1000).to_csv("Sample2.csv")
