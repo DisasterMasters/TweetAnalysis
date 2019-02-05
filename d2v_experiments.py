6de6a05c906ee28bde161806d02c2459b7f9c20c
Nicholas West <nwest13@vols.utk.edu>
2019-02-05 10:10:32 -0500

    Updated d2v_experiment.py (WIP)

--- d2v_experiments.py
+++ d2v_experiments.py
@@ -1,10 +1,10 @@
 import collections
 import re
 import gensim
-from gensim.models.doc2vec import TaggedDocument
-from sklearn.metrics import f1_score
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 class Score(collections.namedtuple("Score", "overall positive negative neutral")):
     def __add__(self, rhs):
@@ -38,7 +38,7 @@ class doc2vec:
 
         if isinstance(Y, basestring):
             df_tags.append(Y)
-        if isinstance(Y, list):
+        elif isinstance(Y, list):
             df_tags = Y
         elif not isinstance(Y, list):
             raise TypeError
@@ -52,6 +52,7 @@ class doc2vec:
 
     def score(self, tokenize_f, verbose=False):
 
+        self.tokenize_f = tokenize_f
         df = self.df
         X = self.x
         Y =self.y
@@ -64,7 +65,7 @@ class doc2vec:
 
         if isinstance(Y, basestring):
             df_tags.append(Y)
-        if isinstance(Y, list):
+        elif isinstance(Y, list):
             df_tags = Y
         elif not isinstance(Y, list):
             raise TypeError
@@ -97,7 +98,7 @@ class doc2vec:
 
         if verbose:
             print("making predictions")
-        test_results = self.predict(test[X])
+        test_results = test[X].map(self.predict_text_main)
         if verbose:
             print("Scoring results")
         print("Label Score: ")
@@ -132,20 +133,12 @@ class doc2vec:
     def predict_text_main(self, document, col=None):  # takes in a string and infers vector and returns vectors and distance
         if col == None:
             col = self.df_tags[0]
-        tokenized_words = re.findall(self.w, document.lower())
+        tokenized_words = self.tokenize_f(document.lower())
         inferred_vector = TaggedDocument(words=tokenized_words, tags=["inferred_vector"])[0]
         inferred_vector = self.model.infer_vector(inferred_vector)
         sims = self.model.docvecs.most_similar([inferred_vector], topn=len(self.model.docvecs))
 #         print([rec for rec in sims if rec[0] in set(self.df[self.df_tags[0]].unique())])
         return [rec for rec in sims if rec[0] in set(self.df[col].unique())][0][0]
-
-    def predict(self, X):  # Takes a series of text and returns a series of predictions
-        if self.verbose:
-            from tqdm import tqdm
-            tqdm.pandas()
-            return X.progress_apply(self.predict_text_main)
-        else:
-            return X.apply(self.predict_text_main)
 
 '''
 
@@ -235,17 +228,93 @@ from nltk.stem.snowball import SnowballStemmer
 stopwords = set(nltk.corpus.stopwords.words("english"))
 stemmer = SnowballStemmer("english")
 
+def d2v_run(
+    df,
+    x,
+    y,
+    tokenize_f,
+    rmstop = False,
+    stem = False,
+    vector_size = 300,
+    window_size = 15,
+    min_count = 1,
+    sampling_threshold = 1e-4,
+    negative_size = 5,
+    train_epoch = 40,
+    dm = 0,
+    worker_count = 7,
+):
+    tag_coloring = {}
+    colors = set(range(len(df)))
+
+    for _, datum in df.iterrows():
+        for tag in datum[y]:
+            if tag not in tag_coloring:
+                tag_coloring[tag] = min(colors - {tag_coloring[x] for x in datum[y] if x in tag_coloring})
+
+    print(tag_coloring)
+
+    training_data, test_data = train_test_split(df, shuffle = True)
+
+    # Split all tags into disjoint sets by elements
+
+    model = Doc2Vec(
+        vector_size = vector_size,
+        window_size = window_size,
+        min_count = min_count,
+        sampling_threshold = sampling_threshold,
+        negative_size = negative_size,
+        train_epoch = train_epoch,
+        dm = dm,
+        worker_count = worker_count
+    )
+
+    training_data_docs = [TaggedDocument(tokenize_f(datum[x]), datum[y]) for _, datum in training_data.iterrows()]
+
+    model.build_vocab(training_data_docs)
+    model.train(training_data_docs, total_examples = model.corpus_count, epochs = model.epochs)
+
+    ys_to_test = {"positive", "negative", "neutral"}
+
+    def infer(x):
+        vec = model.infer_vector(tokenize_f(x))
+        sims = model.docvecs.most_similar([vec], topn = len(model.docvecs))
+
+        return [s for s in sims if s[0] in ys_to_test][0][0]
+
+    def actual(y):
+        return [s for s in y if s in ys_to_test][0]
+
+    inferred_tags = test_data[x].map(infer)
+    actual_tags = test_data[y].map(actual)
+
+    #mlb = MultiLabelBinarizer()
+    #inferred_mlb = mlb.fit_transform(inferred_tags)
+    #actual_mlb = mlb.fit_transform(actual_tags)
+
+    #f1 = f1_score(inferred_mlb, actual_mlb, average = None)
+    #acc = accuracy_score(inferred_mlb, actual_mlb)
+    f1 = f1_score(inferred_tags, actual_tags, average = None)
+    acc = accuracy_score(inferred_tags, actual_tags)
+
+    print(acc)
+    print(f1)
+
 with contextlib.closing(pymongo.MongoClient()) as conn:
     coll = conn["twitter"]["LabeledStatuses_MiscTechCompanies_C"]
-    records = [r["original"] for r in coll.find(projection = ["original"]) if r["original"]["TweetText"] is not None and r["original"]["Sentiment"] != "irrelevant"]
+    records = [r for r in coll.find(projection = ["text", "tags"]) if "irrelevant" not in r["tags"]]
 
-print("Sample size:", len(records))
 df = pd.DataFrame(records)
+df["sentiment"] = df["tags"].map(lambda x: [v for v in x if v in {"positive", "negative", "neutral"}][0])
+df["topic"] = df["tags"].map(lambda x: [v for v in x if v not in {"positive", "negative", "neutral"}][0])
+
+d2v_run(df, "text", "tags", re.compile("\w+", re.I).findall)
+exit()
 
 def score_iter(tokenize_f,  n):
     ret = Score(0, 0, 0, 0)
     for _ in range(n):
-        ret += doc2vec(df, "TweetText", ["Sentiment", "Topic"]).score(tokenize_f, verbose = True)
+        ret += doc2vec(df, "text", ["sentiment", "topic"]).score(tokenize_f, verbose = True)
     ret = Score(
         overall = ret.overall / n,
         positive = ret.positive / n,
